2b3e81d29319bd9a4f3b25d74ac7b8b31c8981c5
Tomasz Strojny <tomasz@tstrojny.pl>
2014-09-23 13:57:15 +0200

    Added DataAccess Find method.

--- comfi.py
+++ comfi.py
@@ -1,31 +1,56 @@
 import os
+import json
+import config                       #import config.py file
+
 class Command:
     def __init__(self, *params):
         self.params = params
         self.command=""
     def os_name(self):
-        if os.name == "posix":
-            uname = os.uname()
-            return uname[0] + " " + uname[2]
-        elif os.name == 'nt':
-            return "Windows"
-        elif os.name == 'os2':
-            return "OSX"
-        else:
-            return "Not supported"
+        pass
     def run(self):
         os.system(self.command)
 
 class DataAccess:
     def __init__(self, commandsFile="./commands.json"):
         self.commandsFile = commandsFile
+        try:
+            self.data = json.load(open(self.commandsFile))
+        except:
+            raise IOError
+
     def find(self, *params):
-        command = params.join(" ")
-        return command
+        """function which looks for command depending on params"""
+        params=list(params)
+        command=self.data
+        i = 0
+        try:
+            while type(command) == dict:
+                if 'type' in command:
+                    break
+                else:
+                    command = command[params[i]]
+                    i = i + 1
+            for command_type, command_prefix in config.types.items():
+                if command_type.decode('utf-8') == command['type']:
+                    command = command_prefix + " " + command[command_type]
+                    break
+            return command
+        except:
+            print("Cannot find command matching to those parameters: %s" % params)
+            raise StandardError
+
     def add(self, command, *params):
         pass
+
     def delete(self, *params):
         pass
 
-command = Command("apache","restart")
-print(command.os_name())
+#command = Command("apache","restart")
+#print(command.os_name())
+try:
+    dataaccess = DataAccess()
+    print(dataaccess.find(config.system, "apache22", "config", "main"))
+except IOError:
+    print("Cannot find commands file")
+
