f38f76135762d946bc4800f94a40176e9c6a1deb
Tomasz Strojny <tomasz@tstrojny.pl>
2014-10-11 19:04:19 +0200

    Command line arguments parsing

--- autoconfig.py
+++ autoconfig.py
@@ -4,4 +4,5 @@ import os                           #to determine operating system
 class Autoconfig:
     def __init__(self):
         print("Autoconfig run")
-
+    def determine_os(self):
+        pass
--- comfi.py
+++ comfi.py
@@ -1,35 +1,16 @@
-import os                           #to determine operating system
 import sys                          #needed to start shell commands from python
 import argparse                     #parsing commandline arguments
 import config                       #import config.py file
 import dataaccess                   #class which is responsible for manipulating the commands_file
+import autoconfig                   #class which can determine some important things and create config file
 
 class CommandLine:
-    def __init__(self, *params):
-        self.params = params
-        self.command=""
-        print(params)
-        #if first run or -a -> run autoconfig
-        #if params[0] == -c -> configfile
-        #if params[0] == -e -> edit command
-        #if params[0] == -d -> delete command
-        #else run command
-
-        #parser = argparse.ArgumentParser(description='Comfortable configurator')
-
-        # try:
-        #     #print(self.params[0])
-        #     if self.params[0] == "-c":
-        #         self.data_access = dataaccess.DataAccess(self.params[1])
-        #         self.params = self.params[2:]
-        #     else:
-        #         self.data_access = dataaccess.DataAccess()
-        # except IOError:
-        #     print("Cannot find commands file")
+    def __init__(self, commands_file, what_to_do, command):
+        self.commands_file = commands_file
+        self.what_to_do = what_to_do
+        self.command = command
+        self.data_access = dataaccess.DataAccess()
                                                                             #TODO do you want to create new commandsfile?\
-
-        #print(self.data_access.find(config.system, "apache22", "config", "main"))
-
     def find_command(self):
         pass
 
@@ -42,16 +23,44 @@ class CommandLine:
     def run_command(self):
         os.system(self.command)
 
-    def determine_os(self):
-        pass
-
 
 parser = argparse.ArgumentParser(description='Comfortable configurator.')
+parser.add_argument('--autoconfig', action='store_true', dest='autoconfig',
+                    default=config.first_run,
+                    help='Run autoconfig script. Other parameters are omitted.')
+
 parser.add_argument('-c', '--commands-file', dest='commands_file',
                     default=config.commands_file,
                     help='Override the path of commands file')
 
-args = parser.parse_args()
-print args.commands_file
+parser.add_argument('-f', '--find',
+                    nargs=argparse.REMAINDER,
+                    help='Edit command that was found using remaining parameters')
+
+parser.add_argument('-a', '--add',
+                    nargs=argparse.REMAINDER,
+                    help='Add command that was found using remaining parameters')
+
+parser.add_argument('-d', '--delete',
+                    nargs=argparse.REMAINDER,
+                    help='Delete command that was found using remaining parameters')
+
+args, command = parser.parse_known_args()
+if command:
+    args.command = command
+else:
+    if args.find:
+        action="find"
+        args.command = args.find
+    elif args.add:
+        action="add"
+        args.command = args.add
+    elif args.delete:
+        action="delete"
+        args.command = args.delete
+    else:
+        action="run"
+
+print (action," ", args.command)
 
 
